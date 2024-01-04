a398284010e86dcd6894eb97b6cd83dc387cc4a1
Jeffery Lofoneh Asamani <jefferyasamani7@gmail.com>
2024-01-04 12:06:59 +0000

    final code

--- Calculator/script.js
+++ Calculator/script.js
@@ -17,3 +17,20 @@ function displayCalculation() {
   document.querySelector('.js-calculation')
     .innerHTML = calculation;
 }
+
+
+function clearCalculation() {
+    calculation = '';
+    displayCalculation();
+    localStorage.removeItem('calculation');
+  }
+  
+function calculateResult() {
+    try {
+      calculation = eval(calculation).toString();
+      displayCalculation();
+      localStorage.setItem('calculation', calculation);
+    } catch (error) {
+      console.error('Invalid input or calculation error:', error);
+    }
+  }
